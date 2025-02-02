4f044c7b5ba8688c4c3899dbf1bd46393ba88441
Kassii <kassidy.satterfield@outlook.com>
2025-02-02 02:28:35 -0600

    Go

---
+++ new.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func add(x, y int) int {
+
+	fmt.Println(x + y)
+	return x + y
+}
+
+func swip(x, y string) (string, string) {
+	return y, x
+}
+
+var o, p = 1, "bitch"
+
+func test() {
+	var a, b, c = 1, 2, 3
+	fmt.Println(a, b, c)
+	fmt.Println(o, p)
+
+}
+
+func main() {
+	fmt.Println("test deos this work?")
+	fmt.Println("new line???")
+	fmt.Println("", rand.Intn(10))
+
+	z := 10
+	v := 10
+
+	x, y := swip("hello", "world")
+	fmt.Println(x, y)
+
+	add(z, v)
+
+	fmt.Println(add(5, 5))
+
+	test()
+}
